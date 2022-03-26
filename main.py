75e5c129d2b08adfad635202606f8637d57485d3
AkashBhasharkar <akashbhasharkar9016@gmail.com>
2022-03-27 00:13:07 +0530

    database created

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     blog_comment = relationship("BlogPost", back_populates = "comments")
 
-# db.create_all()
+db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
